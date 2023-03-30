045c21b3237feeb61bf65daad2a1fe02a5924a17
Henrique Vieira de Jesus <henriquerkn@gmail.com>
2023-03-30 20:00:33 -0300

    Adicionar arquivos de projeto.

---
+++ ListView/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace ListView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxProduto = new System.Windows.Forms.TextBox();
+            this.textBoxQuant = new System.Windows.Forms.TextBox();
+            this.textBoxPreco = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRmv = new System.Windows.Forms.Button();
+            this.buttonObter = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.col_Id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_Prd = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_Qnt = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_Prc = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Stencil", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SlateBlue;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ListView";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.col_Id,
+            this.col_Prd,
+            this.col_Qnt,
+            this.col_Prc});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(18, 46);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(653, 228);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(18, 300);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(100, 20);
+            this.textBoxId.TabIndex = 2;
+            // 
+            // textBoxProduto
+            // 
+            this.textBoxProduto.Location = new System.Drawing.Point(152, 300);
+            this.textBoxProduto.Name = "textBoxProduto";
+            this.textBoxProduto.Size = new System.Drawing.Size(167, 20);
+            this.textBoxProduto.TabIndex = 3;
+            // 
+            // textBoxQuant
+            // 
+            this.textBoxQuant.Location = new System.Drawing.Point(372, 300);
+            this.textBoxQuant.Name = "textBoxQuant";
+            this.textBoxQuant.Size = new System.Drawing.Size(100, 20);
+            this.textBoxQuant.TabIndex = 4;
+            // 
+            // textBoxPreco
+            // 
+            this.textBoxPreco.Location = new System.Drawing.Point(571, 300);
+            this.textBoxPreco.Name = "textBoxPreco";
+            this.textBoxPreco.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPreco.TabIndex = 5;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.buttonAdd.FlatAppearance.BorderColor = System.Drawing.Color.MidnightBlue;
+            this.buttonAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAdd.Font = new System.Drawing.Font("News706 BT", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.Location = new System.Drawing.Point(19, 341);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(198, 40);
+            this.buttonAdd.TabIndex = 6;
+            this.buttonAdd.Text = "Adicionar";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            // 
+            // buttonRmv
+            // 
+            this.buttonRmv.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.buttonRmv.FlatAppearance.BorderColor = System.Drawing.Color.MidnightBlue;
+            this.buttonRmv.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRmv.Font = new System.Drawing.Font("News706 BT", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRmv.Location = new System.Drawing.Point(244, 341);
+            this.buttonRmv.Name = "buttonRmv";
+            this.buttonRmv.Size = new System.Drawing.Size(198, 40);
+            this.buttonRmv.TabIndex = 7;
+            this.buttonRmv.Text = "Remover";
+            this.buttonRmv.UseVisualStyleBackColor = false;
+            // 
+            // buttonObter
+            // 
+            this.buttonObter.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.buttonObter.FlatAppearance.BorderColor = System.Drawing.Color.MidnightBlue;
+            this.buttonObter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonObter.Font = new System.Drawing.Font("News706 BT", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonObter.Location = new System.Drawing.Point(473, 341);
+            this.buttonObter.Name = "buttonObter";
+            this.buttonObter.Size = new System.Drawing.Size(198, 40);
+            this.buttonObter.TabIndex = 8;
+            this.buttonObter.Text = "Obter";
+            this.buttonObter.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SlateBlue;
+            this.label2.Location = new System.Drawing.Point(15, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.SlateBlue;
+            this.label3.Location = new System.Drawing.Point(148, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Produto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.SlateBlue;
+            this.label4.Location = new System.Drawing.Point(368, 277);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Quantidade";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.SlateBlue;
+            this.label5.Location = new System.Drawing.Point(567, 277);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Preço";
+            // 
+            // col_Id
+            // 
+            this.col_Id.Text = "Id";
+            // 
+            // col_Prd
+            // 
+            this.col_Prd.Text = "Produtos";
+            this.col_Prd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.col_Prd.Width = 188;
+            // 
+            // col_Qnt
+            // 
+            this.col_Qnt.Text = "Quantidade";
+            this.col_Qnt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.col_Qnt.Width = 144;
+            // 
+            // col_Prc
+            // 
+            this.col_Prc.Text = "Preço";
+            this.col_Prc.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(720, 409);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonObter);
+            this.Controls.Add(this.buttonRmv);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxPreco);
+            this.Controls.Add(this.textBoxQuant);
+            this.Controls.Add(this.textBoxProduto);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxProduto;
+        private System.Windows.Forms.TextBox textBoxQuant;
+        private System.Windows.Forms.TextBox textBoxPreco;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRmv;
+        private System.Windows.Forms.Button buttonObter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ColumnHeader col_Id;
+        private System.Windows.Forms.ColumnHeader col_Prd;
+        private System.Windows.Forms.ColumnHeader col_Qnt;
+        private System.Windows.Forms.ColumnHeader col_Prc;
+    }
+}
+
